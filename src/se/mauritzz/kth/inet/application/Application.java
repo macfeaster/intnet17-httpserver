64d84e7be4d332386c9dd3418e2e4df9ce88353b
Mauritz Zachrisson <mauritz.zachrisson@gmail.com>
2017-02-01 01:27:11 +0100

    More funzies with templates

--- src/se/mauritzz/kth/inet/application/Application.java
+++ src/se/mauritzz/kth/inet/application/Application.java
@@ -12,9 +12,10 @@ import java.util.Map;
 public class Application implements Process {
 
 	@Override
-	public HttpResponse doGet(HttpRequest request) throws IOException {
+	public HttpResponse doGet(HttpRequest req) throws IOException {
 		Map<String, String> model = new HashMap<>();
 		model.put("variable", "Variable :D");
+		model.put("plopp", req.getQueryString().get("plopp"));
 
 		return new HttpResponse(ResponseType.OK, TemplateEngine.render("start", model));
 	}
